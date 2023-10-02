1504c189285109627a307793ac162f62c6743b9d
Walter Postemsky <wpostes@gmail.com>
2023-10-02 20:22:08 -0300

    Primer Subida

---
+++ script.js
@@ -0,0 +1,14 @@
+@keyframes animate {
+    0%, 100% {
+        background-image: url(media/Abre-la-inscripcion-a-los-cursos-virtuales-del-Centro.jpg);
+        background-size: cover;
+    }
+    33% {
+        background-image: url(media/Abre-la-inscripcion-a-los-cursos-virtuales-del-Centro.jpg);
+        background-size: cover;
+    }
+    66% {
+        background-image: url(media/mujer-auriculares-concentrada-curso-ordenador.jpg);
+        background-size: cover;
+    }
+}
