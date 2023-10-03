a967d3c6ac2de23f9852a30c12334c97ea0f11a0
wpostes <wpostes@gmail.com>
2023-10-02 21:13:52 -0300

    segunda subida

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-@keyframes animate {
-    0%, 100% {
-        background-image: url(media/Abre-la-inscripcion-a-los-cursos-virtuales-del-Centro.jpg);
-        background-size: cover;
-    }
-    33% {
-        background-image: url(media/Abre-la-inscripcion-a-los-cursos-virtuales-del-Centro.jpg);
-        background-size: cover;
-    }
-    66% {
-        background-image: url(media/mujer-auriculares-concentrada-curso-ordenador.jpg);
-        background-size: cover;
-    }
+const carouselContainer = document.querySelector('.carousel-container');
+const slides = document.querySelectorAll('.carousel-slide');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
 }
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 6000); // Cambia automáticamente de imagen cada 3 segundos
+
+showSlide(currentIndex);
+
+carouselContainer.addEventListener('mouseenter', () => {
+    clearInterval(autoSlide);
+});
+
+carouselContainer.addEventListener('mouseleave', () => {
+    autoSlide = setInterval(nextSlide, 6000);
+});
+
+let autoSlide = setInterval(nextSlide, 6000);
